ccc8d23011dec912d0b86d862c9fde3dcac0d87f
JKasakyan <jameskasakyan94@gmail.com>
2016-04-22 20:28:26 -0400

    Moved misplaced comment
    — Docstring moved to inside function

--- skill_huddle/views.py
+++ skill_huddle/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
-"""
-View to serve the home page
-"""
+
 def home(request):
+    """
+    View to serve the home page
+    """
     return render(request, "skill_huddle/index.html")
