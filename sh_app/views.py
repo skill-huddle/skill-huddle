f32c35935d398cf089e6fb39d30c9da79009d0a6
JKasakyan <jameskasakyan94@gmail.com>
2016-04-28 18:28:42 -0400

    Fix docstring format
    — Use pep 8 convention for docstrings

--- sh_app/views.py
+++ sh_app/views.py
@@ -6,16 +6,16 @@ from django.http import HttpResponseRedirect, HttpResponse
 from sh_app.forms import UserForm, SH_User
 
 def index(request):
-    '''
+    """
     Serve view for home page
-    '''
+    """
     return render(request, "index.html")
 
 
 def about(request):
-    '''
+    """
     Serve view for about page
-    '''
+    """
     return render(request, "about.html")
 
 def register(request):
