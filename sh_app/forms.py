32e2456735acc687a56147c1f36b2d2429b9345f
JKasakyan <jameskasakyan94@gmail.com>
2016-04-29 12:05:30 -0400

    Write ModelForm for League
    — Add a LeagueForm

--- sh_app/forms.py
+++ sh_app/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from sh_app.models import User
+from django.forms import Textarea
+from sh_app.models import User, League
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -8,3 +9,10 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username', 'email', 'password')
 
+class LeagueForm(forms.ModelForm):
+    class Meta:
+        model = League
+        fields = ('name', 'description', 'country', 'city', 'state', 'is_private', 'head_official')
+        widgets = {
+            'description': Textarea
+        }
