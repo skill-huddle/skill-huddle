a1b504e923d2c8f7c834709f3c778e79c4db86ae
JKasakyan <jameskasakyan94@gmail.com>
2016-04-29 11:33:27 -0400

    Configure for django-localflavor
    — Added django-localflavor to requirements.txt and add to INSTALLED_APPS

--- skill_huddle/settings.py
+++ skill_huddle/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'sh_app.apps.ShAppConfig',
     'django_countries',
+    'localflavor',
 )
 
 # https://github.com/SmileyChris/django-countries
