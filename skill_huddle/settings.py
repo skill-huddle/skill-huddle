a0baa47d093106a1eb76d874c1e7f7268df81994
JKasakyan <jameskasakyan94@gmail.com>
2016-05-14 21:19:33 -0400

    Update timezone to America/New York

--- skill_huddle/settings.py
+++ skill_huddle/settings.py
@@ -114,7 +114,7 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
